88e5bdbc682cd51ac9e8d63f9563bde11d921b71
Sean <sathshen@163.com>
2026-09-09 20:18:52 +0800

    init utonia

--- models/utonia/__init__.py
+++ models/utonia/__init__.py
@@ -18,9 +18,6 @@ from .model import load
 from . import model
 from . import module
 from . import structure
-from . import data
-from . import transform
 from . import utils
-from . import registry
 
-__all__ = ["load", "model", "module", "structure", "transform", "registry", "utils"]
+__all__ = ["load", "model", "module", "structure", "utils"]
