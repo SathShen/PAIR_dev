f0f757fea702c0467ed79667802ca2cc959e43bd
Sean <sathshen@163.com>
2026-09-10 12:04:52 +0800

    init

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,11 +1,10 @@
 """PAIR dataset package."""
 
+
 from .pair_dataset import (
     DatasetSpec,
     CanonicalChangeTarget,
     UnifiedPAIRDataset,
-    UNKNOWN_CLASS_ID,
-    IGNORE_CLASS_ID,
     infer_unchanged_raw_id,
     infer_binary_class_ids,
 )
